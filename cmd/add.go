a2b50c86379cfc353f84a6c43a62adcad5d8e84d
Patrick Bär <patrick.baer00@gmail.com>
2022-07-18 14:21:38 +0200

    adding add command and improved config module json parsing

--- cmd/add.go
+++ cmd/add.go
@@ -1,11 +1,9 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
 	"fmt"
+	"github.com/baerla/inzidenz-cli/config"
+	"net/url"
 
 	"github.com/spf13/cobra"
 )
@@ -21,7 +19,32 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+		//fmt.Printf("add called, args: %v\n", args)
+		u, err := url.ParseRequestURI(args[1])
+		//fmt.Printf("u: %v ; err: %v \n", u, err)
+		if err != nil {
+			panic(err)
+		}
+
+		city := config.City{
+			Name: args[0],
+			URL:  u.String(),
+		}
+		conf := config.GetConfig()
+		found := false
+		for _, c := range conf.Cities {
+			if c.URL == city.URL && c.Name == city.Name {
+				found = true
+				break
+			}
+		}
+		if !found {
+			conf.Cities = append(conf.Cities, city)
+			conf.SaveToConfigFile()
+			fmt.Printf("City %s with url: %s added", city.Name, city.URL)
+		} else {
+			fmt.Println("Already added")
+		}
 	},
 }
 
--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"os"
 )
 
@@ -14,34 +15,51 @@ type City struct {
 	URL  string `json:"url,omitempty"`
 }
 
+func (c *Config) UnmarshalJSON(b []byte) error {
+	var data map[string]string
+	err := json.Unmarshal(b, &data)
+	check(err)
+
+	for name, url := range data {
+		c.Cities = append(c.Cities, City{Name: name, URL: url})
+	}
+	return nil
+}
+
 var config *Config
 
 func GetConfig() *Config {
 	if config == nil {
-		config = _new()
+		config = importFromConfigFile()
 	}
 	return config
 }
 
-func _new() *Config {
+func importFromConfigFile() *Config {
 	// get file content from env
 	content, err := os.ReadFile(os.Getenv("COVID_INCIDENCE_CONFIG"))
 	check(err)
-	//fmt.Print(string(content))
 
 	// parse content file
-	var data map[string]string
-	err = json.Unmarshal(content, &data)
-	check(err)
-
 	var c Config
-	for name, url := range data {
-		c.Cities = append(c.Cities, City{Name: name, URL: url})
-	}
+	err = json.Unmarshal(content, &c)
+	check(err)
 
 	//fmt.Println(c)
 
 	return &c
+}
+
+func (c *Config) SaveToConfigFile() {
+	// export config to file
+	data := make(map[string]string)
+	for _, city := range c.Cities {
+		data[city.Name] = city.URL
+	}
+	content, err := json.Marshal(data)
+	check(err)
+	err = ioutil.WriteFile(os.Getenv("COVID_INCIDENCE_CONFIG"), content, 0644)
+	check(err)
 }
 
 func check(e error) {
